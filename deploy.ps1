04a225213936d431e6516c064b593ed400085572
aqasrawi <aqasrawi@microsoft.com>
2018-02-07 18:18:40 +0000

    AP Changes

--- deploy.ps1
+++ deploy.ps1
@@ -7,16 +7,26 @@
 #$subscriptionId = '60b6165a-8669-47a2-860c-6ef475127364'
 #Set-AzureRmContext -SubscriptionId $subscriptionId
 $timestamp = Get-Date -Format "yyyy-MM-dd_hh-mm-ss"
-$resourceGroupName = 'GCbluePrintUser01'
+$resourceGroupName = 'GCbluePrintUser04'
 $parametersFile='.\azuredeploy.parameters.json'
 
 #
-# Provision Basic Services / OMS Workspaces
+# Provision Basic Services / OMS Workspaces - ~ 1 hour
 #
 New-AzureRmResourceGroupDeployment -Name "D_$timestamp" -ResourceGroupName $resourceGroupName `
 -TemplateFile .\azuredeploy01.json -TemplateParameterFile $parametersFile `
 -Mode Incremental
 
+#
+# More stuff except for SQL - ~ 26 minutes
+#
 New-AzureRmResourceGroupDeployment -Name "D_$timestamp" -ResourceGroupName $resourceGroupName `
 -TemplateFile .\azuredeploy02.json -TemplateParameterFile $parametersFile `
+-Mode Incremental
+
+#
+# SQL - ~ 1 hr
+#
+New-AzureRmResourceGroupDeployment -Name "D_$timestamp" -ResourceGroupName $resourceGroupName `
+-TemplateFile .\azuredeploy03.json -TemplateParameterFile $parametersFile `
 -Mode Incremental
